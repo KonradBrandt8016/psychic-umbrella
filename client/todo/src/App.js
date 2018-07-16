2aa96dee922d35470525fca47a6a44e8419b8593
Konrad <konrad@typedev.co.za>
2018-07-16 13:32:26 +0200

    Add BrowserRouter to project

--- client/todo/src/App.js
+++ client/todo/src/App.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
-
-import logo from './logo.svg';
-
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   state = {
-    response: ''
+    response: ""
   };
 
   componentDidMount() {
@@ -16,7 +14,7 @@ class App extends Component {
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch("/api/hello");
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
@@ -26,13 +24,14 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">{this.state.response}</p>
+        </div>
+      </BrowserRouter>
     );
   }
 }
