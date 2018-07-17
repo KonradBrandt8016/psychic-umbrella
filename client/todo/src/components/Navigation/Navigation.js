efcd5ba750d1deeb0e3f4ab87c83a70853ec04a3
Konrad <konrad@typedev.co.za>
2018-07-17 10:30:15 +0200

    Add Bulma styling

--- client/todo/src/components/Navigation/Navigation.js
+++ client/todo/src/components/Navigation/Navigation.js
@@ -4,17 +4,21 @@ import { Link } from "react-router-dom";
 
 const navigation = () => {
   return (
-    <ul className="navigation">
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/register">Register</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-    </ul>
+    <section class="hero is-info">
+      <div class="hero-body">
+        <ul className="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
--- client/todo/src/components/Registration/Registration.js
+++ client/todo/src/components/Registration/Registration.js
@@ -1,12 +1,42 @@
 import React from "react";
+import "./Registration.css";
 
 const registration = () => {
   return (
-    <div>
+    <div className="section">
       <form action="/action-page">
-        <input type="text" value="name" required />
-        <input type="email" value="email" required />
-        <input type="password" value="password" required />
+        <div className="container">
+          <div className="tile is-ancestor">
+            <div className="tile is-12">
+              <div className="tile is-parent">
+                <article class="tile is-child notification has-background-light">
+                  <input
+                    class="form-input"
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    required
+                  />
+                  <input
+                    class="form-input"
+                    type="email"
+                    placeholder="Email Address"
+                    name="email"
+                    required
+                  />
+                  <input
+                    class="form-input"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    required
+                  />
+                  <button type="submit">Submit</button>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
