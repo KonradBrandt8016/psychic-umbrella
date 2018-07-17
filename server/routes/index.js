2dc1c805fbbde8ed548f5c34dab02cc182856b76
Leslie Berry <leslie@typedev.co.za>
2018-07-17 12:29:20 +0200

    routing again

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 const taskRoutes = require('./task_routes');
+const userRoutes = require('./user_routes');
 
 module.exports=function(app,db){
     taskRoutes(app,db);
+    userRoutes(app,db);
 }
--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect("mongodb://localhost:27017/integration_test", (err, database) =>{
+MongoClient.connect("mongodb://localhost:27017/ikastDB", (err, database) =>{
     if(err) return console.error(err);
-    db = database.db("ikast-api")
+    db = database.db("ikast-api");
     require('./routes')(app, db);
     //puting the below here so that the app wont start if unable to connect to db
     app.listen(port, () => console.log(`Listening on port ${port}`));
