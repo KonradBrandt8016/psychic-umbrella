e7411caff23567998bd610a88f75bebd7b2dbed4
Konrad <konrad@typedev.co.za>
2018-07-17 11:03:04 +0200

    Branching correction

---
+++ client/todo/src/components/Login/Login.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div className="section">
+      <form action="/action-page">
+        <div className="container">
+          <div className="tile is-ancestor">
+            <div className="tile is-12">
+              <div className="tile is-parent">
+                <article class="tile is-child notification has-background-light">
+                  <input
+                    class="form-input"
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    required
+                  />
+                  <input
+                    class="form-input"
+                    type="email"
+                    placeholder="Email Address"
+                    name="email"
+                    required
+                  />
+                  <input
+                    class="form-input"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    required
+                  />
+                  <button type="submit">Submit</button>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default login;
